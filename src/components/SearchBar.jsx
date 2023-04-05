4a7564b6a5beed9d715d3b9db608687ef1fbc514
josephkiarie1 <62857999+josephkiarie1@users.noreply.github.com>
2023-04-04 20:03:58 -0700

    added sidebar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,12 +6,14 @@ import { Search } from '@mui/icons-material';
 
 const SearchBar = () => {
   return (
-    <Paper component="form" onSubmit={() => {}}
+    <Paper component="form" 
+    onSubmit={() => {}}
     sx={{borderRadius:20, 
         border: '1px solid #e3e3e3',
         pl:2, 
         boxShadow: 'none',  
-        mr:{sm:5}}} 
+        mr:{sm:5},
+    }} 
         >
       <input 
       className="search-bar"
